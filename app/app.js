041837c8280f8bc68d9f95bc28c918dcd64b3869
benshabbat <benshabbat27@gmail.com>
2023-10-13 13:55:36 +0300

    front for socket io

--- app/app.js
+++ app/app.js
@@ -1,21 +1,20 @@
-const socket = new WebSocket('ws://localhost:3000')
+const socket = io("ws://localhost:3000");
 
 function sendMessage(e) {
-    e.preventDefault()
-    const input = document.querySelector('input')
-    if (input.value) {
-        socket.send(input.value)
-        input.value = ""
-    }
-    input.focus()
+  e.preventDefault();
+  const input = document.querySelector("input");
+  if (input.value) {
+    socket.emit("message", input.value);
+    input.value = "";
+  }
+  input.focus();
 }
 
-document.querySelector('form')
-    .addEventListener('submit', sendMessage)
+document.querySelector("form").addEventListener("submit", sendMessage);
 
-// Listen for messages 
-socket.addEventListener("message", ({ data }) => {
-    const li = document.createElement('li')
-    li.textContent = data
-    document.querySelector('ul').appendChild(li)
-})
+// Listen for messages
+socket.on("message", ({ data }) => {
+  const li = document.createElement("li");
+  li.textContent = data;
+  document.querySelector("ul").appendChild(li);
+});
